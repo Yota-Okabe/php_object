f605f9a6dd3ff572141ae7603ff66a150a2f58bd
Yota Okabe <93124377+Yota-Okabe@users.noreply.github.com>
2023-03-28 17:19:22 +0900

    微修正

--- day16_oop/car.php
+++ day16_oop/car.php
@@ -8,7 +8,11 @@
         public $color;
     // 車の為せる技 = メソッド（動詞）
         public function run(){
-            return "高速を走ります";
+            return "が高速を走ります。";
+        }
+        public function distance($speed,$time){
+            $distance = $speed * $time;
+            return $distance;
         }
     }
 
@@ -17,19 +21,28 @@
     $car->company_name = "トヨタ ";
     $car->name = "プリウス";
     echo $car->company_name;
-    echo $car->name . '<br>';
+    echo $car->name;
+    echo $car->run();
+    $distance = $car->distance(40,2);
+    echo "走行距離は" . $distance . "kmです。" . '<br>';
 
     $car1 = new Car();
     $car1->company_name = "日産 ";
-    $car1->name = "リーフ" . '<br>';
+    $car1->name = "リーフ";
     echo $car1->company_name;
     echo $car1->name;
+    echo $car->run();
+    $distance = $car->distance(35,2);
+    echo "走行距離は" . $distance . "kmです。" . '<br>';
 
     $car2 = new Car();
     $car2->company_name = "テスラ ";
-    $car2->name = "Model X" . '<br>';
+    $car2->name = "Model X";
     echo $car2->company_name;
     echo $car2->name;
-
+    echo $car->run();
+    $distance = $car->distance(33,2);
+    echo "走行距離は" . $distance . "kmです。" . '<br>';
 
 ?>
+
---
+++ day16_oop/car2.php
@@ -0,0 +1,46 @@
+<?php
+
+// クラス車を定義
+    class Car{
+    // 車の特徴 = プロパティ（名詞）
+        public $company_name;
+        public $name;
+        public $color;
+    // 車の為せる技 = メソッド（動詞）
+        public function run(){
+            return "が高速を走ります。";
+        }
+        public function distance($speed,$time){
+            $distance = $speed * $time;
+            return $distance;
+        }
+
+// セッターとゲッター
+    // セッター
+        // public function setName($car_name){
+        //     // $this このクラスの
+        //     $this->name = $car_name;
+        // }
+    // ゲッター
+        public function getName(){
+            return "この車は" . $this->name . "です。";
+        }
+// コンストラクタ
+        public function __construct($car_name){
+            $this->name = $car_name;
+        }
+    }
+
+// ゲッターとセッター
+    // $car = new Car();
+    // $car->setName("トヨタ");
+    // echo $car->getName();
+
+    // $car1 = new Car();
+    // $car1->setName("テスラ");
+    // echo $car1->getName();
+
+// コンストラクタ
+    $car = new Car("トヨタ");
+    echo $car->getName();
+?>
