dd964b50efef1c0a297ea622e63d57a448f0026a
Yota Okabe <93124377+Yota-Okabe@users.noreply.github.com>
2023-03-28 13:41:31 +0900

    オブジェクト指向練習

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+    // step1 クラスを定義
+    class Human{
+    // step2 プロパティを記載
+        public $name;
+        public $gender;
+        public $weight;
+
+    // step3 メソッドを記載
+        public function cook(){
+            echo "料理をする";
+        }
+    }
+
+    // step4 インスタンスを生み出す
+    $human = new Human();
+    $human->name = "YOTA";
+    echo $human->name;
+    $human->cook();
+
+
+
+
+
+?>
